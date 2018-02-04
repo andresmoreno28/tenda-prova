5b03f8643598917a86a9d1384ea302a72e9d5961
AMoreno <andresmrubio28@gmail.com>
2018-02-04 23:52:16 +0100

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/* RUTA DE PÀGINA D'USUARIS*/
+Route::get('/usuarios', function() {
+    return 'Aqui aniràn els usuaris';
+});
+
+/* RUTA PROVA PER A MOSTRAR ID D'USUARIS */
+Route::get('/usuarios/{id}', function($id) {
+	return "Mostrant detalls de l'usuari: {$id}";
+	//usuarios/nuevo != usuarios/[0-9]+
+})->where('id', '\d+');  //Expressió regular per a que no coincideixi amb la següent
+
+Route::get('/usuarios/nuevo', function () {
+	return 'Crear nuevo usuario';
+});
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+	$name= ucfirst($name);
+	$nickname= ucfirst($nickname);
+	
+	if ($nickname) {
+		return "Bienvenido {$name}, tu apodo es {$nickname}";
+	} else {
+		return "Bienvenido {$name}";
+	}
+});
